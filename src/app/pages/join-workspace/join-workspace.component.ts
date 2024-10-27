3dadc3c4eb7d46a77e550b1fc1575c6b5bd74aad
Edgars Skrabins <shiftkiro@gmail.com>
2024-10-27 18:26:29 +0200

    Removed old translation logic

--- src/app/pages/join-workspace/join-workspace.component.ts
+++ src/app/pages/join-workspace/join-workspace.component.ts
@@ -3,7 +3,6 @@ import {FormsModule} from "@angular/forms";
 import {AppRoutes} from "../../app/app.routes";
 import {NavigationService} from "../../services/navigation.service";
 import {WorkspaceManagerService} from "../../services/workspaceManager.service";
-import {TranslationService} from "../../services/translations/dictionaries/translations.service";
 import {WorkspaceIdentifyingParams} from "../../services/api.service";
 
 @Component({
@@ -20,7 +19,7 @@ export class JoinWorkspaceComponent {
   workspacePassword = '';
   alertMessage = '';
 
-  constructor(private navigationService: NavigationService, private workspaceManagerService: WorkspaceManagerService, protected translationService: TranslationService) {
+  constructor(private navigationService: NavigationService, private workspaceManagerService: WorkspaceManagerService) {
   }
 
   handleJoinWorkspace() {
