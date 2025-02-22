3fd6047737094eb5e9c2ab009fed2fe667a8abdd
Edgars Skrabins <shiftkiro@gmail.com>
2025-02-22 17:44:39 +0200

    Added translation enum

--- src/app/config/translationConfig.ts
+++ src/app/config/translationConfig.ts
@@ -4,3 +4,15 @@ export const supportedTranslations = [
   'en',
   'lv',
 ]
+
+export const enum TranslationPhrase {
+  Dashboard = "Dashboard",
+  Settings = "Settings",
+  Workspaces = "Workspaces",
+  Profile = "Profile",
+  Login = "Login",
+  Register = "Register",
+  Logout = "Logout",
+  ForgotPassword = "Forgot password",
+  ChangePassword = "Change password"
+}
