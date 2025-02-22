b9e32626b16d77737ebafd847c564b387f374e3f
Edgars Skrabins <shiftkiro@gmail.com>
2025-02-22 19:26:30 +0200

    Drawer styles

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,9 +1,12 @@
 import {Component} from '@angular/core';
+import {DrawerComponent} from "@components/drawer/drawer.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    DrawerComponent
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
