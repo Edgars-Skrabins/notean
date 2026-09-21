c94032fec937f63fd54e1b6e13f75a3830148844
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-09-22 00:53:06 +0300

    removed old workspace-based auth flow

--- src/app/utils/globals.ts
+++
@@ -1,9 +0,0 @@
-import {Workspace} from "../services/workspaceManager.service";
-
-type Globals = {
-  currentWorkspaceData: Workspace | null;
-}
-
-export let globals: Globals = {
-  currentWorkspaceData: null,
-}
