741d0867e057f42523a9e1611d97b6dc2834b22a
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-09-23 13:27:09 +0300

    translated task creation and dashboard text

--- src/app/components/task-creation/task-creation.component.ts
+++ src/app/components/task-creation/task-creation.component.ts
@@ -1,16 +1,21 @@
 import {Component} from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {TranslateModule} from "@ngx-translate/core";
+import {PHRASES} from "@config/phrases";
 
 @Component({
   selector: 'app-task-creation',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    TranslateModule
   ],
   templateUrl: './task-creation.component.html',
   styleUrl: './task-creation.component.css'
 })
 export class TaskCreationComponent {
+  protected readonly PHRASES = PHRASES;
+
   taskName = '';
 
   handleCreateTask() {
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import {Component} from '@angular/core';
+import {TranslateModule} from "@ngx-translate/core";
+import {PHRASES} from "@config/phrases";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [
+    TranslateModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-
+  protected readonly PHRASES = PHRASES;
 }
