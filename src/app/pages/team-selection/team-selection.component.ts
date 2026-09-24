2d48cf982d162ad429aaccbc53cb7501020abdcc
Edgars Skrabiņš <shiftkiro@gmail.com>
2026-09-24 14:25:56 +0300

    added a back-to-dashboard button on the team selection page for users who already have a team

--- src/app/pages/team-selection/team-selection.component.ts
+++ src/app/pages/team-selection/team-selection.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {TranslateModule} from "@ngx-translate/core";
+import {NgIf} from "@angular/common";
 import {NavigationService} from "@services/navigation.service";
+import {TeamService} from "@services/team.service";
 import {AppRoutes} from "../../app/app-routes.enum";
 import {PHRASES} from "@config/phrases";
 import {AuthCardComponent} from "@components/auth-card/auth-card.component";
@@ -10,6 +12,7 @@ import {AuthCardComponent} from "@components/auth-card/auth-card.component";
   standalone: true,
   imports: [
     TranslateModule,
+    NgIf,
     AuthCardComponent
   ],
   templateUrl: './team-selection.component.html',
@@ -18,7 +21,14 @@ import {AuthCardComponent} from "@components/auth-card/auth-card.component";
 export class TeamSelectionComponent {
   protected readonly PHRASES = PHRASES;
 
-  constructor(private navigationService: NavigationService) {
+  constructor(
+    private navigationService: NavigationService,
+    private teamService: TeamService
+  ) {
+  }
+
+  get hasTeam(): boolean {
+    return this.teamService.getCurrentTeam() !== null;
   }
 
   handleGoToCreateTeam() {
@@ -27,5 +37,9 @@ export class TeamSelectionComponent {
 
   handleGoToJoinTeam() {
     this.navigationService.navigate(AppRoutes.JOIN_TEAM);
+  }
+
+  handleGoBack() {
+    this.navigationService.navigate(AppRoutes.DASHBOARD);
   }
 }
